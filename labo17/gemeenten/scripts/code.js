aae79e5217b2a9c89744d480939289f9c8a07191
Elias-Ostyn <elias.ostyn@student.vives.be>
2025-03-11 21:45:31 +0100

    labo17

---
+++ labo17/gemeenten/scripts/code.js
@@ -0,0 +1,28 @@
+const setup = () => {
+    sortgemeentes()
+
+
+}
+
+const sortgemeentes = () => {
+    const gemeente = [];
+
+    let input = prompt("geef een paar steden" )
+
+    while (input !== "stop") {
+        gemeente.push(input);
+        input = prompt("geef een paar steden" );
+    }
+
+    gemeente.sort()
+
+    const drop = document.getElementById("dropdownlijst");
+
+    gemeente.forEach((gemeente) => {
+        drop.add(new Option(gemeente))
+    })
+}
+
+
+
+window.addEventListener("load", setup);
