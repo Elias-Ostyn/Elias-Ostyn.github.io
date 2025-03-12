fe071952cc0fa77f90104a8dbdbfe19046019ae5
Elias-Ostyn <elias.ostyn@student.vives.be>
2025-03-12 12:07:57 +0100

    l

--- labo16/forms/scripts/code.js
+++ labo16/forms/scripts/code.js
@@ -1,11 +1,5 @@
 const setup = () => {
-    // Kleurtje kiezen
-    let colorPicker = document.getElementById("fav-color");
-    let colorPreview = document.getElementById("color-preview");
 
-    colorPicker.addEventListener("input", () => {
-        colorPreview.style.backgroundColor = colorPicker.value;
-    });
     // die slider voor getal errnaast te tonen
     let rangeSlider = document.getElementById("big-fav-number");
     let rangeValue = document.getElementById("range-value");
